678fddfa25886c3c27fd25ce3ea6157f078056cf
Etienne Baudry <etienne@webmaestro.fr>
2017-02-19 22:34:09 -0500

    Placeholder

--- static/js/client.js
+++ static/js/client.js
@@ -4,5 +4,7 @@ var player = new JSMpeg.Player(url + ":8082", {
     canvas: canvas,
     loop: false,
     autoplay: true,
-    audio: false
+    audio: false,
+    poster: "http://placehold.it/640x480/222222/ffffff?text=Waiting+for+camera+...",
+    progressive: false
 });
