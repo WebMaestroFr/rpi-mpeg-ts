e6ef4e50859f1311767bcf609d634a1874453349
Etienne Baudry <etienne@webmaestro.fr>
2017-02-21 09:05:59 -0500

    Install script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
+[[ "$(whoami)" != "root" ]] && echo "Please run script as root or sudo." && exit
+
 curl -sL https://deb.nodesource.com/setup_6.x | bash -
 
 apt-get upgrade -y -q
 apt-get install git nodejs libav-tools -y
+
+npm install
